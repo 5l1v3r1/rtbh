709828b07246a1052d703cc6836a24319ce70580
R.P. Aditya <aditya@grot.org>
2012-11-26 08:44:29 -0500

    indent change only

--- bin/fetchBlockLists.pl
+++ bin/fetchBlockLists.pl
@@ -10,51 +10,52 @@ my($git) = "/usr/local/bin/git --git-dir=${rtbh_home}/.git --work-tree=${rtbh_ho
 require "${rtbh_home}/bin/common.pl";
 
 my($lists);
-while(<>){
-	next if (/(^$)|(^#)/);
-	chomp;
-	my($type, $blockUrl) = split;
-	$lists->{$blockUrl} = $type;
+while (<>) {
+  next if (/(^$)|(^#)/);
+  chomp;
+  my($type, $blockUrl) = split;
+  $lists->{$blockUrl} = $type;
 }
 
-for my $url (keys %{$lists}){
-	my($outfile) = $url;
-	my($listType) = $lists->{$url};
-	$outfile =~ s/^\/etc\///;
-	$outfile =~ s/\//./g;
-	my($outdir) = ${rtbh_home} . "/etc/" . $listType;
-	if (! -d $outdir){
-	  notify("debug", "creating dir ${outdir}");
-	  print STDERR `/bin/mkdir -p ${outdir}`;
-	  if ($?){
-	    notify("err", "could not mkdir ${outdir}: ${?}");
-	    next;
-	  }
-	}
-	$outfile = $outdir . "/" . $outfile;
-	if (-r $outfile){
-		my($lastfetch) = getFileCreateTime($outfile);
-		my($ttl) = 300000;
-		my($now) = time;
-		my($diff) = $now - $lastfetch;
-		if ($diff < $ttl){
-			notify('info', "skipping fetch of ${url} fetched $diff secs ago");
-			next;
-		}
-	} else {
-		print STDERR `/usr/bin/touch ${outfile}`;
-	}
-	    notify('info',  " retrieving filter from ${url}");
-	    print STDERR  `/usr/bin/fetch -i ${outfile} -T 10 -o ${outfile} ${url}`;
-	    my($logmsg) = "fetched version at " . time;
-	if ($outfile =~ /\.bz2$/){
-		my $uncompressed_outfile = $outfile;
-		$uncompressed_outfile =~ s/\.bz2$//g;
-		print STDERR `/bin/rm ${uncompressed_outfile}`;
-		print STDERR `/usr/bin/bunzip2 ${outfile}`;
-		$outfile = $uncompressed_outfile;
-	}
-	    print STDERR `${git} diff ${outfile}`;
-	    print STDERR `${git} add ${outfile}`;
-	    print STDERR `${git} commit -m"${logmsg}" ${outfile}`;
+for my $url (keys %{$lists}) {
+  my($outfile) = $url;
+  my($listType) = $lists->{$url};
+  $outfile =~ s/^\/etc\///;
+  $outfile =~ s/\//./g;
+  my($outdir) = ${rtbh_home} . "/etc/" . $listType;
+  if (! -d $outdir) {
+    notify("debug", "creating dir ${outdir}");
+    print STDERR `/bin/mkdir -p ${outdir}`;
+    if ($?) {
+      notify("err", "could not mkdir ${outdir}: ${?}");
+      next;
+    }
+  }
+  $outfile = $outdir . "/" . $outfile;
+  if (-r $outfile) {
+    my($lastfetch) = getFileCreateTime($outfile);
+    my($ttl) = 300000;
+    my($now) = time;
+    my($diff) = $now - $lastfetch;
+    if ($diff < $ttl) {
+      notify('info', "skipping fetch of ${url} fetched $diff secs ago");
+      next;
+    }
+  } else {
+    print STDERR `/usr/bin/touch ${outfile}`;
+  }
+  notify('info',  " retrieving filter from ${url}");
+  print STDERR  `/usr/bin/fetch -i ${outfile} -T 10 -o ${outfile} ${url}`;
+  my($logmsg) = "fetched version at " . time;
+
+  if ($outfile =~ /\.bz2$/) {
+    my $uncompressed_outfile = $outfile;
+    $uncompressed_outfile =~ s/\.bz2$//g;
+    print STDERR `/bin/rm ${uncompressed_outfile}`;
+    print STDERR `/usr/bin/bunzip2 ${outfile}`;
+    $outfile = $uncompressed_outfile;
+  }
+  print STDERR `${git} diff ${outfile}`;
+  print STDERR `${git} add ${outfile}`;
+  print STDERR `${git} commit -m"${logmsg}" ${outfile}`;
 }
